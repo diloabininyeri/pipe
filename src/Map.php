eec9720c313c7e48f35d2e759e9426bd22e9966f
Dilo surucu <berxudar@gmail.com>
2025-04-23 01:18:19 -0400

    Additional methods were added to the map class, such as default and transform.

--- src/Map.php
+++ src/Map.php
@@ -11,16 +11,21 @@ class Map
 {
 
     /**
-     * @var Closure
+     * @var Closure $closure
      */
     private Closure $closure;
+
+    /**
+     * @var array $transformers
+     */
+    private array $transformers = [];
 
     /**
      *
      */
     public function __construct()
     {
-        $this->closure=static fn(mixed $value)=>$value;
+        $this->closure = static fn(mixed $value) => $value;
     }
 
     /**
@@ -28,7 +33,7 @@ class Map
      * @param Closure $closure
      * @return $this
      */
-    public function add(Closure $closure):self
+    public function add(Closure $closure): self
     {
         $previous = $this->closure;
         $this->closure = static fn($value) => $closure($previous($value));
@@ -40,17 +45,73 @@ class Map
      * @param array $array
      * @return array
      */
-    public function apply(array $array):array
+    public function apply(array $array): array
     {
+        foreach ($this->transformers as $transformer) {
+            $array = array_map($transformer, $array);
+        }
         return array_map($this->closure, $array);
     }
 
     /**
-     * @noinspection PhpUnused
+     * @param Closure $transform
+     * @return $this
+     */
+    public function transform(Closure $transform): self
+    {
+        $this->transformers[] = $transform;
+        return $this;
+    }
+
+    /**
+     * @param string $delimiter
+     * @return $this
+     */
+    public function explode(string $delimiter): self
+    {
+        return $this->transform(function ($value) use ($delimiter) {
+            return explode($delimiter, $value);
+        });
+    }
+
+    /***
+     * @param string $delimiter
+     * @return self
+     */
+    public function implode(string $delimiter): self
+    {
+        return $this->transform(function ($value) use ($delimiter) {
+            return implode($delimiter, $value);
+        });
+    }
+
+    /**
+     * @param string $callable
+     * @return self
+     */
+    public function callback(string $callable): self
+    {
+        return $this->transform(fn($value) => $callable($value));
+    }
+
+    /***
+     * @param bool $withTransformers
      * @return Closure
      */
-    public function getClosure(): Closure
+    public function getClosure(bool $withTransformers = true): Closure
     {
+        if ($withTransformers) {
+            return fn(array $array) => $this->apply($array);
+        }
         return $this->closure;
+    }
+
+    /**
+     * @param mixed $default
+     * @return $this
+     */
+    public function default(mixed $default): self
+    {
+        return $this->transform(fn($value) => $value ?? $default);
     }
 }
